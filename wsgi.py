fa2508566a87fd9f6af613ee99410da5161b684d
Jeffrey Hogan <jhogan@hashicorp.com>
2022-01-31 15:29:34 -0600

    More logging tweaks

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@
 import os
 import logging
 
-import click
 
 from member_card import create_app
 from google.cloud.logging_v2.handlers._helpers import get_request_data
@@ -42,29 +41,27 @@ class MemberCardFormatter(GoogleCloudFormatter):
         if inferred_trace is not None and self.gcp_project is not None:
             # add full path for detected trace
             inferred_trace = f"projects/{self.gcp_project}/traces/{inferred_trace}"
-        return {
+        entry = {
             "timestamp": self.format_timestamp(record.asctime),
             "severity": self.format_severity(record.levelname),
             "message": record.getMessage(),
             "labels": self.make_labels(),
             "metadata": self.make_metadata(record),
             "sourceLocation": self.make_source_location(record),
-            "logging.googleapis.com/trace": inferred_trace,
-            "span_id": inferred_span,
-            "http_request": inferred_http,
         }
-
-
-class RemoveColorFilter(logging.Filter):
-    def filter(self, record):
-        if record and record.msg and isinstance(record.msg, str):
-            record.msg = click.unstyle(record.msg)
-        return True
+        if inferred_trace:
+            entry.update(
+                {
+                    "logging.googleapis.com/trace": inferred_trace,
+                    "span_id": inferred_span,
+                    "http_request": inferred_http,
+                }
+            )
+        return entry
 
 
 log_level = os.getenv("LOG_LEVEL", "INFO").upper()
 logging_handlers = ["default"]
-logging_handlers = ["json"]
 running_on_cloud_run = "K_SERVICE" in os.environ
 if running_on_cloud_run:
     logging_handlers = ["json"]
@@ -72,11 +69,6 @@ if running_on_cloud_run:
 dictConfig(
     {
         "version": 1,
-        "filters": {
-            "no_color": {
-                "()": RemoveColorFilter,
-            }
-        },
         "formatters": {
             "json": {
                 "()": "wsgi.MemberCardFormatter",
@@ -99,7 +91,6 @@ dictConfig(
             "json": {
                 "class": "logging.StreamHandler",
                 "formatter": "json",
-                "filters": ["no_color"],
             },
         },
         "loggers": {
@@ -116,54 +107,6 @@ dictConfig(
     }
 )
 
-# logging_handlers = ["default"]
-# logging_handlers = ["gcloud"]
-# if running_on_cloud_run := "K_SERVICE" in os.environ:
-#     logging_handlers = ["gcloud"]
-# dictConfig(
-#     {
-#         "version": 1,
-#         # "filters": {
-#         #     "no_color": {
-#         #         "()": RemoveColorFilter,
-#         #     }
-#         # },
-#         "formatters": {
-#             # "json": {
-#             #     "()": "google.cloud.logging.handlers.StructuredLogHandler",
-#             #     "application_info": {
-#             #         "type": "python-application",
-#             #         "name": "digital-membership",
-#             #     },
-#             #     "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
-#             # },
-#             "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
-#         },
-#         "handlers": {
-#             "default": {
-#                 "level": "INFO",
-#                 "formatter": "standard",
-#                 "class": "logging.StreamHandler",
-#                 "stream": "ext://sys.stdout",
-#             },
-#             "gcloud": {
-#                 "class": StructuredLogHandler(),
-#                 # "formatter": "json",
-#             },
-#         },
-#         "loggers": {
-#             "": {
-#                 "level": os.getenv("LOG_LEVEL", "INFO").upper(),
-#                 "handlers": logging_handlers,
-#                 "propagate": False,
-#             },
-#             "member_card": {
-#                 "level": os.getenv("LOG_LEVEL", "INFO").upper(),
-#                 "handlers":  logging_handlers,
-#             },
-#         },
-#     }
-# )
 logger = logging.getLogger(__name__)
 
 
