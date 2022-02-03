74ba3eb2b643358bb16e79391429589363360a6c
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-02 19:45:01 -0600

    min scale issue?

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -6,5 +6,7 @@ gunicorn \
   --bind=0.0.0.0:8080 \
   --log-file=- \
   --log-level=info \
+  --timeout=0 \
+  --preload \
   --log-config=config/gunicron_logging.ini \
   'wsgi:create_app()'
--- terraform/cloud_run_service.tf
+++ terraform/cloud_run_service.tf
@@ -34,7 +34,7 @@ resource "google_cloud_run_service" "digital_membership" {
 
     metadata {
       annotations = {
-        "autoscaling.knative.dev/minScale"      = "0"
+        "autoscaling.knative.dev/minScale"      = "1"
         "autoscaling.knative.dev/maxScale"      = "1"
         "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.digital_membership.connection_name
         "run.googleapis.com/client-name"        = each.key
