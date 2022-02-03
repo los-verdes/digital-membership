d4eee8090316848f0e556e7235301a12c24c2ec9
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-03 16:56:05 -0600

    bah

--- terraform/cloud_run_service.tf
+++ terraform/cloud_run_service.tf
@@ -40,10 +40,11 @@ resource "google_cloud_run_service" "digital_membership" {
 
     metadata {
       annotations = {
-        "autoscaling.knative.dev/minScale"      = "0"
-        "autoscaling.knative.dev/maxScale"      = "1"
-        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.digital_membership.connection_name
-        "run.googleapis.com/client-name"        = each.key
+        "autoscaling.knative.dev/minScale"         = "0"
+        "autoscaling.knative.dev/maxScale"         = "1"
+        "run.googleapis.com/cloudsql-instances"    = google_sql_database_instance.digital_membership.connection_name
+        "run.googleapis.com/client-name"           = each.key
+        "run.googleapis.com/execution-environment" = "gen2"
       }
     }
 
