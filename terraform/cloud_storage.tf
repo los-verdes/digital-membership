6601fb42aa3d46252a3fa3bcf33985f7c75a1d56
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-03 14:48:07 -0600

    tidy up

--- terraform/cloud_storage.tf
+++ terraform/cloud_storage.tf
@@ -10,6 +10,8 @@ resource "google_storage_bucket" "statics" {
   }
 }
 
+# TODO: fix all these IAM bits here...
+
 resource "google_storage_bucket_iam_member" "all_users_viewers" {
   bucket = google_storage_bucket.statics.name
   role   = "roles/storage.legacyObjectReader"
@@ -49,31 +51,3 @@ resource "google_storage_bucket_iam_member" "digital_membership_worker_sa_bucket
   role   = "roles/storage.legacyBucketReader"
   member = "serviceAccount:${google_service_account.digital_membership["worker"].email}"
 }
-
-
-# resource "google_storage_bucket_iam_member" "test_site_publisher_sa_obj_admin" {
-#   bucket = google_storage_bucket.static_site.name
-#   role   = "roles/storage.objectAdmin"
-#   member = "serviceAccount:${google_service_account.test_site_publisher.email}"w
-
-#   # condition {
-#   #   title       = "tests-prefix-only"
-#   #   description = "Only allow object admin under a tests/ prefix"
-#   #   expression  = <<-expression
-#   #     resource.name.startsWith(“projects/_/buckets/${google_storage_bucket.static_site.name}/objects/tests”)
-#   #     expression
-#   # }
-# }
-
-# data "cloudflare_zone" "static_site" {
-#   name = var.cloudflare_zone
-# }
-
-# resource "cloudflare_record" "static_site" {
-#   zone_id = data.cloudflare_zone.static_site.id
-#   name    = var.static_site_hostname
-#   value   = "c.storage.googleapis.com"
-#   type    = "CNAME"
-#   ttl     = 1
-#   proxied = true
-# }
