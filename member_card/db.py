013fb78490c823ab187ab1ef60f454932a082a51
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-22 13:29:16 -0600

    migrate GCP SQL connector bits to more relevant locale...

--- member_card/db.py
+++ member_card/db.py
@@ -1,12 +1,62 @@
 #!/usr/bin/env python
 import logging
+from functools import partial
+from typing import TYPE_CHECKING
 
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from google.cloud.sql.connector import connector
+
+if TYPE_CHECKING:
+    from pg8000 import dbapi
 
 db = SQLAlchemy()
 migrate = Migrate()
 logger = logging.getLogger(__name__)
+
+
+def get_gcp_sql_engine_creator(instance_connection_string, settings):
+    db_connection_kwargs = dict(
+        instance_connection_string=instance_connection_string,
+        db_user=settings.DB_USERNAME,
+        db_name=settings.DB_DATABASE_NAME,
+        db_pass=settings.DB_PASSWORD,
+    )
+    redacted_db_kwargs = {
+        k: f"{v[:3]}...{v[-3:]}"
+        for k, v in db_connection_kwargs.items()
+        if v is not None
+    }
+
+    logger.debug(
+        f"Using {redacted_db_kwargs} with GCP SQL cloud connector...",
+        extra=redacted_db_kwargs,
+    )
+
+    def get_db_connector(
+        instance_connection_string: str, db_user: str, db_name: str, db_pass: str
+    ) -> "dbapi.Connection":
+        conn_kwargs = dict(
+            user=db_user,
+            db=db_name,
+            # enable_iam_auth=True,
+        )
+
+        conn_obj = connector.Connector()
+        if db_pass:
+            conn_kwargs["password"] = db_pass
+        else:
+            # TODO: drop this kludge pending release of https://github.com/GoogleCloudPlatform/cloud-sql-python-connector/pull/273
+            conn_obj._enable_iam_auth = not db_pass
+        conn: "dbapi.Connection" = conn_obj.connect(
+            instance_connection_string,
+            "pg8000",
+            **conn_kwargs,
+        )
+        return conn
+
+    engine_creator = partial(get_db_connector, **db_connection_kwargs)
+    return engine_creator
 
 
 def get_or_update(session, model, filters, kwargs):
