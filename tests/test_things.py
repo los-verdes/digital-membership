2673f1e7b36ca883b0313b558b2b8ce6997eb0b8
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-22 15:55:51 -0600

    first batch of trivial, but working!, tests

--- tests/test_things.py
+++ tests/test_things.py
@@ -1,8 +1,10 @@
+import logging
+
 import pytest
 from member_card import create_app
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def app():
     app = create_app(env="tests")
     app.config.update(
@@ -28,6 +30,17 @@ def runner(app):
     return app.test_cli_runner()
 
 
-def test_request_example(client):
-    response = client.get("/posts")
-    assert b"<h2>Hello, World!</h2>" in response.data
+class TestUnauthenticatedRequests:
+    def test_home_redirects_to_login_page(self, client):
+        response = client.get("/")
+        logging.debug(f"{response=}")
+        # Check that we've been redirected to the login page:
+        assert response.status_code == 302
+        assert response.location == "http://localhost/login?next=%2F"
+
+    def test_login_page(self, client):
+        response = client.get("/login")
+        assert b'<form id="emailDistributionRequestForm"' in response.data
+        assert b' id="appleid-signin" ' in response.data
+        assert b' id="yahoo-oauth2-button" ' in response.data
+        assert b' id="google-oauth2-button" ' in response.data
