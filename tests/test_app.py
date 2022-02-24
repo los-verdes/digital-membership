f666aa587345961d240a7c5b32528c9a447e174c
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-24 15:56:26 -0600

    lil commit_on_success tests...

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 import logging
 
+from member_card.app import commit_on_success
+
 
 class TestUnauthenticatedRequests:
     def test_home_redirects_to_login_page(self, client):
@@ -27,3 +29,34 @@ class TestAuthenticatedRequests:
         with app.app_context():
             assert current_login_user
         assert b"los.verdes.tester@gmail.com" in response.data
+
+
+def test_db_commit_on_teardown(app, client, mocker):
+    from member_card.db import db
+
+    mock_session = mocker.patch.object(db, "session")
+
+    mock_session.start()
+
+    client.get("/")
+
+    mock_session.commit.assert_called_once()
+    mock_session.rollback.assert_not_called()
+    mock_session.remove.assert_called()
+    mock_session.stop()
+
+
+def test_db_teardown_rollback_on_error(client, mocker):
+    from member_card.db import db
+    assert client
+
+    mock_session = mocker.patch.object(db, "session")
+
+    mock_session.start()
+    logging.warning(f"test_db_error_on_teardown(): {id(mock_session)=}")
+    commit_on_success(Exception("just for testing tho"))
+
+    mock_session.commit.assert_not_called()
+    mock_session.rollback.assert_called_once()
+    mock_session.remove.assert_called()
+    mock_session.stop()
