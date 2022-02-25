1ac2deb3887094586811595a0dd34e7c05050c16
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-25 11:00:30 -0600

    Speed up test_initialize_tracer

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -8,15 +8,13 @@ if TYPE_CHECKING:
 
 def test_initialize_tracer(mocker: "MockerFixture"):
     mock_trace = mocker.patch("member_card.monitoring.trace")
-    mock_trace.start()
-
     mock_trace_provider = mocker.patch("member_card.monitoring.TracerProvider")
-    mock_trace_provider.start()
+
+    # Also mock out a few other calls; just to keep test case runtimes down...
+    mocker.patch("member_card.monitoring.set_global_textmap")
+    mocker.patch("member_card.monitoring.CloudTraceSpanExporter")
 
     initialize_tracer()
     mock_trace.set_tracer_provider.assert_called_once_with(
         mock_trace_provider.return_value
     )
-
-    mock_trace_provider.stop()
-    mock_trace.stop()
