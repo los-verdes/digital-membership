983bace8e33ba9c9216e4de9f7863f8c1f609627
Jeffrey Hogan <jhogan@hashicorp.com>
2022-02-25 15:49:03 -0600

    tests for edit user name route

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,15 @@
 import logging
+from typing import TYPE_CHECKING
 from urllib.parse import quote_plus
 
 from bs4 import BeautifulSoup
 from flask.testing import FlaskClient
+from member_card import utils
 from member_card.app import commit_on_success
+from member_card.models.user import User
+
+if TYPE_CHECKING:
+    from flask import Flask
 
 
 def ensure_login_required(client: "FlaskClient", path, method="GET"):
@@ -25,6 +31,14 @@ class TestUnauthenticatedRequests:
         assert b' id="appleid-signin" ' in response.data
         assert b' id="yahoo-oauth2-button" ' in response.data
         assert b' id="google-oauth2-button" ' in response.data
+
+    def test_edit_user_name_invalid_method(self, client: "FlaskClient"):
+        response = client.get("/edit-user-name")
+        # Check that this method is indeed _not_ allowed
+        assert response.status_code == 405
+
+    def test_edit_user_name(self, client: "FlaskClient"):
+        ensure_login_required(client, path="/edit-user-name", method="POST")
 
 
 class TestAuthenticatedRequests:
@@ -59,6 +73,43 @@ class TestAuthenticatedRequests:
             soup.find(id="membership-primary-info-text").text.strip()
             == fake_member.fullname
         )
+
+    def test_edit_user_name_by_inactive_member(
+        self, app: "Flask", authenticated_client: "FlaskClient", fake_user: "User"
+    ):
+        self.assert_edit_user_name_success(app, authenticated_client, fake_user)
+
+    def test_edit_user_name_by_active_member(
+        self, app: "Flask", authenticated_client: "FlaskClient", fake_member: "User"
+    ):
+        self.assert_edit_user_name_success(app, authenticated_client, fake_member)
+
+    def assert_edit_user_name_success(self, app, authenticated_client, fake_user):
+        new_first_name = "You done"
+        new_last_name = "Been Edited"
+        new_fullname = f"{new_first_name} {new_last_name}"
+        response = authenticated_client.post(
+            "/edit-user-name",
+            data=dict(
+                newFirstName=new_first_name,
+                newLastName=new_last_name,
+            ),
+            follow_redirects=True,
+        )
+        assert response.status_code == 200
+
+        soup = BeautifulSoup(response.data.decode("utf-8"), "html.parser")
+        form_message_element = soup.find(id="form-message")
+        assert form_message_element
+        assert form_message_element.text.strip() == utils.get_message_str(
+            "edit_user_name_success"
+        )
+
+        with app.app_context():
+            updated_fake_user = User.query.filter_by(id=fake_user.id).one()
+        assert updated_fake_user.first_name == new_first_name
+        assert updated_fake_user.last_name == new_last_name
+        assert updated_fake_user.fullname == new_fullname
 
 
 def test_db_commit_on_teardown(app, client, mocker):
