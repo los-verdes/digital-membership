af518c1023b31b688e7d41f2ca88a00f69bd22da
Jeffrey Hogan <jhogan@hashicorp.com>
2022-03-07 11:26:58 -0600

    invoke test runner bits w/ full args rather than objs

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,24 +1,22 @@
-from member_card import commands
-
 from typing import TYPE_CHECKING
+
 
 if TYPE_CHECKING:
     from flask.testing import FlaskCliRunner
-
     from member_card.models import User
 
 
 class TestCommands:
     def test_query_db_no_user_match(self, runner: "FlaskCliRunner"):
         result = runner.invoke(
-            cli=commands.query_db,
-            args=["this-user-aint-here"],
+            args=["query-db", "this-user-aint-here"],
         )
+        assert result.exit_code == 0
         assert result.return_value is None
 
     def test_query_db_user_match(self, runner: "FlaskCliRunner", fake_user: "User"):
         result = runner.invoke(
-            cli=commands.query_db,
-            args=[fake_user.email],
+            args=["query-db", fake_user.email],
         )
+        assert result.exit_code == 0
         assert fake_user.email in result.output
