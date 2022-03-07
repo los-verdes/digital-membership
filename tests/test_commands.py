788c7be8df93a99a5c0148869d37e67fa8f70cc3
Jeffrey Hogan <jhogan@hashicorp.com>
2022-03-07 11:39:18 -0600

    test_sync_subscriptions

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -4,9 +4,28 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from flask.testing import FlaskCliRunner
     from member_card.models import User
+    from pytest_mock.plugin import MockerFixture
 
 
 class TestCommands:
+    def test_sync_subscriptions(
+        self, runner: "FlaskCliRunner", mocker: "MockerFixture"
+    ):
+        # mock_worker = mocker.patch("member_card.commands.worker")
+        mock_sync_subs = mocker.patch("member_card.commands.worker").sync_subscriptions_etl
+
+        result = runner.invoke(
+            args=["sync-subscriptions", "--load-all"],
+        )
+
+        assert result.exit_code == 0
+        assert result.return_value is None
+
+        mock_sync_subs.assert_called_once_with(
+            message=dict(type="cli-sync-subscriptions"),
+            load_all=True,
+        )
+
     def test_query_db_no_sqlalchemy(self, runner_without_db: "FlaskCliRunner"):
         # TODO: contrived thing for getting a conditional branch covered. Can prob be moved elsewere or dropped eventually....
         result = runner_without_db.invoke(
@@ -25,5 +44,6 @@ class TestCommands:
         result = runner.invoke(
             args=["query-db", fake_user.email],
         )
+
         assert result.exit_code == 0
         assert fake_user.email in result.output
