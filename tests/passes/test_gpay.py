7f7208ed25146611b9d76212e36dfd8853c9c96a
Jeffrey Hogan <jhogan@hashicorp.com>
2022-03-11 11:38:44 -0600

    passes :100: woohoo!

--- tests/passes/test_gpay.py
+++ tests/passes/test_gpay.py
@@ -5,6 +5,7 @@ from member_card.passes import gpay
 
 if TYPE_CHECKING:
     from flask import Flask
+    from member_card.models import MembershipCard
     from pytest_mock.plugin import MockerFixture
 
 
@@ -43,3 +44,112 @@ class TestGooglePassJwt:
 
         mock_jwt_google.encode.assert_called_once()
         assert signed_jwt == mock_jwt_google.encode.return_value
+
+
+@pytest.fixture()
+def pay_client_mock(app: "Flask", mocker: "MockerFixture") -> gpay.GooglePayApiClient:
+    mock_service_account = mocker.patch("member_card.passes.gpay.service_account")
+    mock_auth_session = mocker.patch("member_card.passes.gpay.AuthorizedSession")
+    with app.app_context():
+        client = gpay.new_client()
+    mock_service_account.Credentials.from_service_account_file.assert_called_once()
+    return dict(
+        client=client,
+        mock_service_account=mock_service_account,
+        mock_auth_session=mock_auth_session,
+    )
+
+
+class TestGooglePayApiClient:
+    def test_init(self, pay_client_mock: gpay.GooglePayApiClient):
+        assert pay_client_mock["client"]
+
+    def test_request(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].request(
+            method="GET",
+            resource_type="test-resource_type",
+            resource_id="test-resource_id",
+        )
+        assert response
+
+    def test_request_with_json_payload(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].request(
+            method="GET",
+            resource_type="test-resource_type",
+            resource_id="test-resource_id",
+            json_payload=dict(type="test"),
+        )
+        assert response
+
+    def test_get_pass_class(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].get_pass_class(
+            class_id="test-class_id",
+        )
+        assert response
+
+    def test_get_pass_object(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].get_pass_object(
+            object_id="test-object_id",
+        )
+        assert response
+
+    def test_insert_class(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].insert_class(
+            class_id="test-class_id",
+            payload=dict(),
+        )
+        assert response
+
+    def test_insert_object(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].insert_object(
+            object_id="test-object_id",
+            payload=dict(),
+        )
+        assert response
+
+    def test_patch_class(self, pay_client_mock: gpay.GooglePayApiClient):
+        response = pay_client_mock["client"].patch_class(
+            class_id="test-class_id",
+            payload=dict(),
+        )
+        assert response
+
+    def test_modify_pass_class_patch(self, app: "Flask", mocker: "MockerFixture"):
+        mock_new_client = mocker.patch("member_card.passes.gpay.new_client")
+        with app.app_context():
+            response = gpay.modify_pass_class(operation="patch")
+        assert response
+        mock_new_client.return_value.patch_class.assert_called_once()
+
+    def test_modify_pass_class_insert(self, app: "Flask", mocker: "MockerFixture"):
+        mock_new_client = mocker.patch("member_card.passes.gpay.new_client")
+        with app.app_context():
+            response = gpay.modify_pass_class(operation="insert")
+        assert response
+        mock_new_client.return_value.insert_class.assert_called_once()
+
+    # def test_patch_object(self, pay_client_mock: gpay.GooglePayApiClient):
+    #     response = pay_client_mock["client"].patch_object(
+    #         object_id="test-object_id",
+    #         payload=dict(),
+    #     )
+    #     assert response
+
+
+class TestGeneratePassJwt:
+    def test_generate_pass_jwt(
+        self, app: "Flask", fake_card: "MembershipCard", mocker: "MockerFixture"
+    ):
+        mock_new_google_pass_jwt = mocker.patch(
+            "member_card.passes.gpay.new_google_pass_jwt"
+        )
+        mock_new_client = mocker.patch("member_card.passes.gpay.new_client")
+        mock_gpay_client = mock_new_client.return_value
+
+        with app.app_context():
+            result = gpay.generate_pass_jwt(membership_card=fake_card)
+
+        assert result
+
+        mock_new_google_pass_jwt.assert_called_once()
+        mock_gpay_client.insert_object.assert_called_once()
