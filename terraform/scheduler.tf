96b7ed02a111182cfacc2673d2028a3e11183d9e
Jeffrey Hogan <jhogan@hashicorp.com>
2023-02-22 12:22:53 -0600

    also sync things every hour

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -1,7 +1,7 @@
 resource "google_cloud_scheduler_job" "sync_subscriptions_etl" {
   name        = "sync-subscriptions-etl"
   description = "Regularly recurring Squarespace order into membership database ETL task"
-  schedule    = "0 */6 * * *"
+  schedule    = "0 * * * *"
 
   pubsub_target {
     topic_name = google_pubsub_topic.digital_membership.id
