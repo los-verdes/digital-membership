2b5a0659563cc081f6b8c9e8126d1dd78ae99a23
Jeffrey Hogan <jhogan@hashicorp.com>
2023-02-22 13:52:49 -0600

    note to self!

--- terraform/bootstrap/secrets.tf
+++ terraform/bootstrap/secrets.tf
@@ -9,4 +9,12 @@ resource "google_secret_manager_secret" "digital_membership" {
 resource "google_secret_manager_secret_version" "digital_membership" {
   secret      = google_secret_manager_secret.digital_membership.id
   secret_data = var.app_secret_data
+
+  lifecycle {
+    # 1. first tf state rm this resource
+    # 2. deploy updated version with a tf apply
+    # 3. kick off the main deployment pipeline to update the cloud run services to the newly created version
+    # 4. (manually) go clean up the version `state rm`'ed in step #1 :)
+    prevent_destroy = true
+  }
 }
