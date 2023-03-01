d0c51fe74988fdc28d858655c17ddd252211e5dd
Jeffrey Hogan <jhogan@hashicorp.com>
2023-03-01 13:18:26 -0600

    initial slack stubs

--- member_card/models/__init__.py
+++ member_card/models/__init__.py
@@ -2,6 +2,7 @@
 from member_card.models.annual_membership import AnnualMembership
 from member_card.models.apple_device_registration import AppleDeviceRegistration
 from member_card.models.membership_card import MembershipCard
+from member_card.models.slack_user import SlackUser
 from member_card.models.squarespace_webhook import SquarespaceWebhook
 from member_card.models.table_metadata import TableMetadata
 from member_card.models.user import Role, User
@@ -15,6 +16,7 @@ __all__ = (
     "MembershipCard",
     "User",
     "Role",
+    "SlackUser",
     "SquarespaceWebhook",
     "Store",
     "StoreUser",
