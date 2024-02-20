5c2938e5021c4845d3531fd83eebf657a551330d
Jeffrey Hogan <jhogan@hashicorp.com>
2024-02-20 15:36:04 -0600

    slack ETL is actually fine; customer ETL is the culprit

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -3,7 +3,6 @@ locals {
     run_slack_members_etl = {
       description = "Sync Slack member / user data into local digital membership database"
       schedule    = "0 */6 * * *"
-      paused      = true
       data = {
         type = "run_slack_members_etl",
       }
@@ -11,6 +10,7 @@ locals {
     sync_customers_etl = {
       description = "Sync E-commerce customer / user data into local digital membership database"
       schedule    = "30 * * * *"
+      paused      = true
       data = {
         type = "sync_customers_etl",
       }
@@ -18,7 +18,6 @@ locals {
     sync_subscriptions_etl = {
       description = "Regularly recurring Squarespace order into membership database ETL task"
       schedule    = "0 * * * *"
-      paused      = true
       data = {
         type = "sync_subscriptions_etl",
       }
