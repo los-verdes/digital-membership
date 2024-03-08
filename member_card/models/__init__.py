63077b64ce2c09c5b5ed1706d4e58d899e351f4b
Jeffrey Hogan <jhogan@hashicorp.com>
2024-03-08 13:58:24 -0600

    Add subscription model

--- member_card/models/__init__.py
+++ member_card/models/__init__.py
@@ -1,14 +1,16 @@
 # from member_card.db import Model  # Base, Table
+from social_flask_sqlalchemy import models
+
 from member_card.models.annual_membership import AnnualMembership
 from member_card.models.apple_device_registration import AppleDeviceRegistration
 from member_card.models.membership_card import MembershipCard
 from member_card.models.slack_user import SlackUser
 from member_card.models.squarespace_webhook import SquarespaceWebhook
-from member_card.models.table_metadata import TableMetadata
-from member_card.models.user import Role, User
 from member_card.models.store import Store
 from member_card.models.store_user import StoreUser
-from social_flask_sqlalchemy import models
+from member_card.models.subscription import Subscription
+from member_card.models.table_metadata import TableMetadata
+from member_card.models.user import Role, User
 
 __all__ = (
     "AnnualMembership",
@@ -20,6 +22,7 @@ __all__ = (
     "SquarespaceWebhook",
     "Store",
     "StoreUser",
+    "Subscription",
     "TableMetadata",
     "models",
 )
